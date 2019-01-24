01d6d51381fea2efd8aa60afcf9fae6b04275227
joel123vilca <joevil23tjq@gmail.com>
2019-01-23 23:13:42 -0500

    vuex init

---
+++ src/components/ProductList.vue
@@ -0,0 +1,29 @@
+<template>
+     <div>
+         <h1>List Products</h1>
+         <ul>
+             <li v-for="product in products" :key="product.id"> {{product.title}}.{{product.price}}
+             </li>
+         </ul>
+     </div>
+</template>
+
+<script>
+import shop from '@/api/shop.js';
+export default {
+    data(){
+        return {
+            products: []
+        }
+    },
+    created () {
+        shop.getProducts(products => {
+            this.products = products
+        });
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
